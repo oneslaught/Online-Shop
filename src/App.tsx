af6c778fe9ef446efa3a60c57a300d2de3850bec
oneslaught <iceminecraft021@gmail.com>
2024-05-22 18:45:26 +0300

    created

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+import Banner from "./components/Banner";
+import "./styles/App.css";
+
+export default function App() {
+  return (
+    <>
+      <Banner />
+    </>
+  );
+}
---
+++ webpack.config.ts
@@ -0,0 +1,54 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import { Configuration } from "webpack";
+import "webpack-dev-server";
+
+const webpackConfig: Configuration = {
+  devServer: {
+    compress: true,
+    port: 9000,
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+  },
+  entry: "./src/index.tsx",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  performance: {
+    maxAssetSize: 512 * 1024 * 2,
+    maxEntrypointSize: 512 * 1024 * 2,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  stats: {
+    children: true,
+  },
+};
+
+export default webpackConfig;
