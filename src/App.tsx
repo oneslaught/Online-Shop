3231865df8f6789a90fe439854b6b784ecdbdc19
oneslaught <iceminecraft021@gmail.com>
2024-06-21 23:13:06 +0300

    new products component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as React from "react";
 
-// import ImageCarousel from "./components/ImageCarousel";
-// import Navbar from "./components/Navbar";
-// import NewProducts from "./components/NewProducts";
-import NewSlider from "./components/NewSlider";
+import ImageCarousel from "./components/ImageCarousel";
+import Navbar from "./components/Navbar";
+import NewProducts from "./components/NewProducts";
+// import NewSlider from "./components/NewSlider";
 import "./styles/app.css";
 
 export default function App() {
   return (
     <>
-      {/* <Navbar />
+      <Navbar />
       <ImageCarousel />
-      <NewProducts /> */}
-      <NewSlider />
+      <NewProducts />
+      {/* <NewSlider /> */}
     </>
   );
 }
--- src/components/NewProducts.tsx
+++ src/components/NewProducts.tsx
@@ -1,14 +1,96 @@
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import React from "react";
 
+import bed from "../assets/hattie_bed_frame.jpg?as=webp";
+import chairs from "../assets/saville_dining_chairs.jpeg?as=webp";
+import sofa from "../assets/sofa_angel.jpg?as=webp";
+import * as styles from "../styles/new_products.module.css";
+
 export default function NewProducts() {
   return (
-    <Box sx={{ m: "90px 0 90px 0" }}>
-      <Box sx={{ m: "0 200px 50px 200px" }}>
+    <Box sx={{ m: "90px 0" }}>
+      <Box>
         <Typography sx={{ fontFamily: '"Lato", sans-serif', textAlign: "center" }} variant="h2">
           New products
         </Typography>
+        <Box className={styles.product}>
+          <NavigateBeforeIcon className={styles.prev_btn} />
+          <NavigateNextIcon className={styles.next_btn} />
+          <Box className={styles.product_container}>
+            <Box className={styles.product_card}>
+              <Box className={styles.product_image}>
+                <Box className={styles.discount_tag} component="span">
+                  50% off
+                </Box>
+                <img alt="" className={styles.product_thumb} src={chairs} />
+                <button className={styles.card_btn}>add to cart</button>
+              </Box>
+              <Box className={styles.product_info}>
+                <Typography className={styles.product_brand} variant="h2">
+                  brand
+                </Typography>
+                <Typography className={styles.product_des} paragraph>
+                  Lorem ipsum dolor sit amet.
+                </Typography>
+                <Box className={styles.price} component="span">
+                  20$
+                </Box>
+                <Box className={styles.actual_price} component="span">
+                  40$
+                </Box>
+              </Box>
+            </Box>
+            <Box className={styles.product_card}>
+              <Box className={styles.product_image}>
+                <Box className={styles.discount_tag} component="span">
+                  50% off
+                </Box>
+                <img alt="" className={styles.product_thumb} src={sofa} />
+                <button className={styles.card_btn}>add to cart</button>
+              </Box>
+              <Box className={styles.product_info}>
+                <Typography className={styles.product_brand} variant="h2">
+                  brand
+                </Typography>
+                <Typography className={styles.product_des} paragraph>
+                  Lorem ipsum dolor sit amet.
+                </Typography>
+                <Box className={styles.price} component="span">
+                  20$
+                </Box>
+                <Box className={styles.actual_price} component="span">
+                  40$
+                </Box>
+              </Box>
+            </Box>
+            <Box className={styles.product_card}>
+              <Box className={styles.product_image}>
+                <Box className={styles.discount_tag} component="span">
+                  50% off
+                </Box>
+                <img alt="" className={styles.product_thumb} src={bed} />
+                <button className={styles.card_btn}>add to cart</button>
+              </Box>
+              <Box className={styles.product_info}>
+                <Typography className={styles.product_brand} variant="h2">
+                  brand
+                </Typography>
+                <Typography className={styles.product_des} paragraph>
+                  Lorem ipsum dolor sit amet.
+                </Typography>
+                <Box className={styles.price} component="span">
+                  20$
+                </Box>
+                <Box className={styles.actual_price} component="span">
+                  40$
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
