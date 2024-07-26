625af5b6956dc4b306c0ae87c1ca4a3b764831d5
oneslaught <iceminecraft021@gmail.com>
2024-07-26 23:56:04 +0300

    updated

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,9 +1,8 @@
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import React from "react";
 
-import image from "../assets/about_us_bg.jpg?as=webp";
+import image from "../assets/about_us.jpg?as=webp";
 import * as styles from "../styles/about_us.module.css";
 
 export default function AboutUs() {
@@ -21,7 +20,11 @@ export default function AboutUs() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, maxime? Explicabo doloremque, unde omnis possimus, recusandae
             mollitia sunt, doloribus quisquam sit consequuntur eligendi harum facilis reiciendis porro. Facere, blanditiis. Optio.
           </Typography>
-          <Button disableRipple>Learn more</Button>
+          <button className={styles.content_btn}>
+            <Typography className={styles.content_btn_text} component="span">
+              Learn more
+            </Typography>
+          </button>
         </Box>
       </Box>
     </Box>
